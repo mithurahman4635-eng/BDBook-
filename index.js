76a5fa5c9c706eb6fc08df920f141ff3bd9f5d41
mithurahman4635-eng <mithurahman4635@gmail.com>
2026-04-08 04:03:16 +0800

    final: delete server.js and fix 404

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// ফাইলগুলো কোথায় আছে তা নিশ্চিত করা
 app.use(express.static(__dirname));
 
+// সরাসরি ফাইল পাঠানোর ব্যবস্থা
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+// অন্য সব লিঙ্কেও যাতে 404 না আসে
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'login.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server is Live'));
+app.listen(PORT, () => console.log('Server is Live on ' + PORT));
